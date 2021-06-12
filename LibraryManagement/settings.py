1c0a18346a7f249b6c49611ce4a1d1ba1c4c28cf
Mahdi Namaki <mavenium@gmail.com>
2021-06-12 21:22:39 +0430

    add InitializeAdmin to INSTALLED_APPS

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'UserManagement.apps.UsermanagementConfig'
+    'UserManagement.apps.UsermanagementConfig',
+    'InitializeAdmin.apps.InitializeAdminConfig',
 ]
 
 MIDDLEWARE = [
