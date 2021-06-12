7e972454b191573cfa0b1d6eec14b338de2f9d10
Mahdi Namaki <mavenium@gmail.com>
2021-06-12 21:16:26 +0430

    add Auth Settings

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -136,3 +136,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth Settings
+AUTH_USER_MODEL = 'Accounting.User'
