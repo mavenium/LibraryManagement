8563e02b023ada675a5b981fb0da40c5923e58ae
Mahdi Namaki <mavenium@gmail.com>
2021-06-12 23:10:22 +0430

    add books_create url

--- API/urls.py
+++ API/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('v1/publishers/', views.PublisherList.as_view(), name="publishers"),
     path('v1/books_by_author/<int:pk>/', views.BookByAuthorList.as_view(), name="books_by_author"),
     path('v1/books_by_publisher/<int:pk>/', views.BookByPublisherList.as_view(), name="books_by_publisher"),
+    path('v1/books_create/', views.BookCreate.as_view(), name="books_create"),
 ]
