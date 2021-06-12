ee29dcf9a7b413e7068d6fb9d1cdecb47f9c4421
Mahdi Namaki <mavenium@gmail.com>
2021-06-12 22:47:32 +0430

    add publishers url

--- API/urls.py
+++ API/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('v1/authors/', views.AuthorList.as_view(), name="authors"),
+    path('v1/publishers/', views.PublisherList.as_view(), name="publishers"),
 ]
