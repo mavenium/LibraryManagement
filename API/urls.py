e5d270834b7ab030f2c81848110b23841788f5dc
Mahdi Namaki <mavenium@gmail.com>
2021-06-12 22:58:00 +0430

    add books_by_author url

--- API/urls.py
+++ API/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('v1/authors/', views.AuthorList.as_view(), name="authors"),
     path('v1/publishers/', views.PublisherList.as_view(), name="publishers"),
+    path('v1/books_by_author/<int:pk>/', views.BookByAuthorList.as_view(), name="books_by_author"),
 ]
